8f527ad9276d81dfc80d9c29e8d57b2369426703
Caeden Webb <caedenwebb@unomaha.edu>
2023-04-18 17:22:22 -0500

    Version 4

---
+++ test_account.py
@@ -0,0 +1,28 @@
+import pytest
+from account import Account
+
+def test_init():
+    a1 = Account('Caeden')
+    assert a1.get_balance() == 0
+    assert a1.get_name() == 'Caeden'
+def test_deposit():
+    a1 = Account('Caeden')
+    assert a1.deposit(200) == True
+    assert a1.deposit(0) == False
+    assert a1.deposit(-100) == False
+
+def test_withdraw():
+    a1 = Account('Caeden')
+    a1.deposit(100)
+    assert a1.withdraw(0) == False
+    assert a1.withdraw(-100) == False
+    assert a1.withdraw(100) == True
+    assert a1.withdraw(1000) == False
+
+def test_get_name():
+    a1 = Account('Caeden')
+    a1.get_name() == 'Caeden'
+def test_get_balance():
+    a1 = Account('Caeden')
+    a1.deposit(100)
+    assert a1.get_balance() == 100
