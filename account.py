38b475f2e08901adcb60256d95aee2c7eb478ebd
Caeden Webb <caedenwebb@unomaha.edu>
2023-04-26 14:33:13 -0500

    Docstring for init

--- account.py
+++ account.py
@@ -1,5 +1,10 @@
 class Account:
     def __init__(self, name):
+        '''
+        Creates a new account for money to be deposited or withdrawn. Balance starts at $0.
+        :param name: This is the name on the account.
+        '''
+
         self.__account_name = name
         self.__account_balance = 0
 
