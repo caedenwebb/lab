909c6e59e07b7abced10b7a3d62f2d884985acdd
Caeden Webb <caedenwebb@unomaha.edu>
2023-04-26 14:39:56 -0500

    Checking account balance on test

--- test_account.py
+++ test_account.py
@@ -8,16 +8,23 @@ def test_init():
 def test_deposit():
     a1 = Account('Caeden')
     assert a1.deposit(200) == True
+    assert a1.deposit == 200
     assert a1.deposit(0) == False
+    assert a1.deposit == 200
     assert a1.deposit(-100) == False
+    assert a1.deposit == 200
 
 def test_withdraw():
     a1 = Account('Caeden')
     a1.deposit(100)
     assert a1.withdraw(0) == False
+    assert a1.get_balance() == 100
     assert a1.withdraw(-100) == False
+    assert a1.get_balance == 100
     assert a1.withdraw(100) == True
+    assert a1.get_balance() == 0
     assert a1.withdraw(1000) == False
+    assert a1.get_balance() == 0
 
 def test_get_name():
     a1 = Account('Caeden')
